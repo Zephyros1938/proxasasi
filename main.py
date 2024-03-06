4c6d6944daf85144d196b13017824de2374d047e
Xtrau505 <xtrau505true@gmail.com>
2024-03-05 19:46:15 -0600

    Push existing project to GitLab

---
+++ main.py
@@ -0,0 +1,154 @@
+import os
+
+import flask
+import requests
+from flask import Flask, request, send_from_directory
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+PORTNUMB = 80
+ADDRNUMB = '45.25.249.174'
+
+COMBINEPORTADDR = str(ADDRNUMB + ":" + str(PORTNUMB))
+
+
+def write_favicon(data_to_write):
+    try:
+        with open('static/favicon.ico', 'wb') as favicon_file:
+            favicon_file.write(data_to_write.__bytes__())
+    except Exception as e:
+        print(f"Error writing favicon.ico: {e}")
+
+
+def remove_occurences_in_list(LIST: list, STRING: str, WHERETOREMOVE: int):
+    # print("STRING = " + STRING)
+    COUNT = 0
+    for i in range(0, len(LIST) - 1):
+        if STRING == LIST[i]:
+            COUNT += 1
+            if COUNT == WHERETOREMOVE:
+                del LIST[i]
+                break
+
+
+def remove_occurences_in_string(STRING: str, WHATTOREMOVE: str, SPLIT: str, WHERETOREMOVE: int):
+    LIST = STRING.split(SPLIT)
+    print(LIST)
+    remove_occurences_in_list(LIST, WHATTOREMOVE, WHERETOREMOVE)
+    print(LIST)
+    RETURNTHIS = reconstruct_list_to_string(LIST)
+    print(RETURNTHIS)
+    return RETURNTHIS
+
+
+def reconstruct_list_to_string(LIST):
+    # print("----- ----- ----- ----- -----")
+    TEMPSTRING = ''
+    for i in range(len(LIST)):
+        TEMPSTRING += LIST[i]
+        TEMPSTRING += "/"
+        # print(TEMPSTRING)
+    # print("----- ----- ----- ----- -----")
+    return TEMPSTRING
+
+
+def modify_html_content(content, full_url, method, url):
+    # print("Starting modification of HTML content...")
+    # print("Full URL:", full_url)
+    # print("Method:", method)
+    # print("Base URL:", url)
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    # Convert relative URLs starting with '/' to proxied URLs
+    for tag in soup.find_all():
+        for attr in tag.attrs:
+            if isinstance(tag[attr], str) and tag[attr].startswith('/'):
+                original_url = tag[attr]
+                proxied_url = f'http://127.0.0.1:5000/{method}/{url}{original_url}'
+                # print("Original URL:", original_url)
+                # print("Proxied URL:", proxied_url)
+                tag[attr] = proxied_url
+                # print("Current URL:", tag[attr], "\n")
+                #remove_occurences_in_string(tag[attr], '127.0.0.1:5000', '/', 2)
+
+    # Convert URLs starting with http:// or https:// to proxied URLs
+    for tag in soup.find_all():
+        for attr in tag.attrs:
+            if isinstance(tag[attr], str) and (
+                    tag[attr].startswith('http://') or tag[attr].startswith('https://') and not tag[attr].startswith(
+                '127.0.0.1:5000/http://')):
+                original_url = tag[attr]
+                if original_url.startswith("http://"):
+                    original_url = original_url[7:]
+                elif original_url.startswith("https://"):
+                    original_url = original_url[8:]
+                proxied_url = f'http://127.0.0.1:5000/{method}/{original_url}'
+                # print("Original URL:", original_url)
+                # print("Proxied URL:", proxied_url)
+                tag[attr] = proxied_url
+                # print("Current URL:", tag[attr], "\n")
+                #remove_occurences_in_string(tag[attr], '127.0.0.1:5000', '127.0.0.1:5000', 2)
+
+    # print("Modification completed.")
+    return str(soup)
+
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(directory=os.path.join(app.root_path, 'static'), path='favicon.ico', max_age=None)
+
+
+@app.route('/')
+def root():
+    return open("static/Welcome.html")
+
+
+@app.route('/<method>/<path:url>')
+def proxy(url, method):
+    base_url = urlparse(request.base_url).hostname
+
+    # print("BASE URL = " + base_url)
+    # print(request.base_url.split('/', 600))
+    # print("BASE URL = " + base_url)
+    if "127.0.0.1" in base_url:
+        full_url_pre_split = f'{url}'
+    else:
+        full_url_pre_split = f'{method}//{base_url}//{url}'
+    full_url_split = full_url_pre_split.split('/', 500)
+    remove_occurences_in_list(full_url_split, url, 1)
+    full_url = method + "//" + reconstruct_list_to_string(full_url_split)
+    # print(full_url_split)
+    # print("FULL URL = " + full_url)
+    """if request.method == 'GET':"""
+    if True:
+        try:
+            response = requests.request(method=request.method, url=full_url)
+            if url.split('/')[0] == "127.0.0.1:5000":
+                favicon_url = f'{method}//' + url.split('/')[2] + "//favicon.ico"
+            else:
+                favicon_url = f'{method}//' + url.split('/')[0] + "//favicon.ico"
+            #print(favicon_url)
+            # print( url.split('/'))
+            favicon_load = requests.request(method=request.method, url=favicon_url)
+            # print("URL for favicon:", f'{method}//{url}/favicon.ico')
+            if favicon_load.status_code == 200:
+                # print("Favicon content received:")
+                write_favicon(favicon_load.content)
+            else:
+                # print("Failed to fetch favicon.ico:", favicon_load.status_code)
+                pass
+
+            modified_content = modify_html_content(response.content.decode('utf-8', errors='ignore'), full_url, method, url)
+            # print(modified_content)
+            return modified_content
+        except requests.exceptions.RequestException as e:
+            return str(e), 500
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
